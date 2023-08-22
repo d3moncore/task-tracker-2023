5c946bf77df82a41d7b4a57c271797b45bda51e1
d3moncore <sabarish.padmakumar10@gmail.com>
2023-08-23 03:27:31 +0530

    initial commit with button and header components.

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title}) => {
+    const onClick = () => {
+        console.log('clicked');
+    }
+    return (
+    <header className='header'>
+        <h1>
+            {title}
+        </h1>
+        <Button color="magenta" text="Add" onClick={onClick}/>
+    </header>
+  );
+}
+
+Header.defaultProps = {
+    title: "Task Tracker"
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+
+// const headerStyle = {
+//     color: "steelblue", backgroundColor: "chartreuse"
+// }
+export default Header
